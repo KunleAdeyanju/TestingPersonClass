d7343a81726091b685a96547c2bf7665b750735e
Kunle <kunle@Zipcoders-MacBook-Pro-5.local>
2025-03-08 12:45:33 -0500

    Got on a role and passed all the tests

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -4,32 +4,44 @@ package com.zipcodewilmington.person;
  * If you did ObjectsOne lab, you might recognize this code?
  */
 public class Person {
-    private String name;
-    private int age;
+    private String name = "";
+    private int age = Integer.MAX_VALUE;
 
     public Person() {
+
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.age = age;
+        this.name = name;
+
     }
 
     public void setName(String name) {
+        new Person(name);
+        this.name = name;
     }
 
     public void setAge(int age) {
+        new Person(age);
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+
+        return age;
     }
 }
